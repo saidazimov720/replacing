4d28ee800909363ae2b01cac82e4d698071d0d1c
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-07-25 15:14:00 +0500

    remove the block of code

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function replace() {
     const replace = document.getElementById('replace').value;
     const word = document.getElementById('word').value;
 
-    if (text === "" || replace === "" || word ==="") {
-        alert("Barcha bo'limlarni to'liq to'ldiring.")
-        return;
-    }
+    // if (text === "" || replace === "" || word ==="") {
+    //     alert("Barcha bo'limlarni to'liq to'ldiring.")
+    //     return;
+    // }
     const textreplace = text.replace(new RegExp(replace, 'gi'), word );
 
     document.getElementById("result").innerHTML = textreplace;
